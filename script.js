9ab3b5ed8052394d8701a239dd940c900a0fd9a2
Aditya Basak <adityabasak7031@gmail.com>
2024-08-22 21:09:38 +0530

    fix: display hide and block functionality

--- script.js
+++ script.js
@@ -2,38 +2,54 @@ const formElement = document.querySelector('form');
 const dateInput = document.querySelector('#last-period');
 const cycleLenthInput = document.querySelector('#cycle-length');
 const submitBtn = document.querySelector('#btn');
-const resultDiv = document.querySelector('#result')
+const resultDiv = document.querySelector('#result');
 
 
-function calculatePeriodDate() {
+function calculatePeriodDate(isUserAction = false) {
+
+
+    const savedDate = localStorage.getItem('lastPeriodDate');
+    const savedCycle = localStorage.getItem('cycleLength');
+
+    if (savedDate && savedCycle) {
+        dateInput.value = savedDate;
+        cycleLenthInput.value = savedCycle;
+    }
 
     let lastPeriodDate = new Date(dateInput.value);
     let cycle = parseInt(cycleLenthInput.value);
 
+
     // Mosly Logic won't come here for the required filed in input
-    if (isNaN(lastPeriodDate.getTime()) || isNaN(cycle)) {
-        alert(`Please Enter Valid number!`)
+
+    if (isUserAction && (isNaN(lastPeriodDate.getTime()) || isNaN(cycle))) {
+        alert('Please enter valid data and cycle length!');
         return;
     }
 
-    let nextPeriodDate = new Date(lastPeriodDate);
-    nextPeriodDate.setDate(lastPeriodDate.getDate() + cycle);
+    if (!isNaN(lastPeriodDate.getTime()) && !isNaN(cycle)) {
+        let nextPeriodDate = new Date(lastPeriodDate);
+        nextPeriodDate.setDate(lastPeriodDate.getDate() + cycle);
 
+        resultDiv.style.display = 'block';
 
-    resultDiv.style.display = 'block';
-    resultDiv.innerHTML = `Next Period date: ${nextPeriodDate.toDateString()}`;
+        let showDate = nextPeriodDate.toDateString();
+        resultDiv.innerHTML = `Next Period date: ${showDate}`;
+    }
 
+    localStorage.setItem('lastPeriodDate', dateInput.value);
+    localStorage.setItem('cycleLength', cycleLenthInput.value);
 
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    calculatePeriodDate();
+
+});
 
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    calculatePeriodDate();
+    calculatePeriodDate(true);
 
-})
-
-
-
-// calculatePeriodDate();
+});
